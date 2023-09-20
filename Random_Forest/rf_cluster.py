86dc9510d50d7ee34790d1520309bc94a0d38248
Moncada-Francesco-97 <110817494+Moncada-Francesco-97@users.noreply.github.com>
2023-09-20 10:31:39 +0200

    Update to check if python version works on the cluster

--- Random_Forest/rf_cluster.py
+++ Random_Forest/rf_cluster.py
@@ -3,16 +3,23 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 import os
-import pandas as pd
 import seaborn as sns
 import fiona
 import sklearn
-
 from sklearn.model_selection import KFold
-
-import xgboost as xgb
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import KFold
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
+
+#Upload the selection of glacier i will use in the model
+selection = []
+
+with open('/Users/francesco/Desktop/Thesis/Data/dataset_filtered' + '/selecao.txt', 'r') as f:
+    for line in f:
+        selection.append(int(line.strip()))
+
+common_years = np.arange(2005,2017,1)
+
+print('The shape of the selection is: ', np.shape(selection))
--- Random_Forest/rf_functions.py
+++ Random_Forest/rf_functions.py
@@ -6,19 +6,20 @@ from sklearn.model_selection import GridSearchCV
 
 #Thiis function is used to get the best parameters for the random forest 
 
-def rf_train_and_fit(X, y, cv_split, param_grid):
+def rf_train_and_fit(X, y, cv_split, param_grid, scoring_criterium):
 
     '''Parameters:
     X: the features
     y: the target
     cv_split: the cross validation split
-    param_grid: the grid of parameters to test'''
+    param_grid: the grid of parameters to test
+    scoring_criterium: the scoring criterium to use for the grid search'''
 
     #defining the model
     rf = RandomForestRegressor(random_state=42, n_jobs=-1)
 
     #Perform the grid search
-    grid_search = GridSearchCV(estimator=rf, param_grid= param_grid, cv=cv_split, verbose=2, n_jobs=-1, scoring='neg_mean_squared_error') #lower values are better
+    grid_search = GridSearchCV(estimator=rf, param_grid= param_grid, cv=cv_split, verbose=2, n_jobs=-1, scoring= str(scoring_criterium)) #lower values are better
 
     print('The parameters to test are: ', param_grid, '\n')
     #'The cross validation score is : ', grid_search.best_score_, '\n'
